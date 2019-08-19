546b72670485d00a855c24808c137f94dc6d6b8d
Matias Grynberg Portnoy <43388395+mgp123@users.noreply.github.com>
2019-08-19 17:01:17 -0300

    agregado triangulacion simple. No tiene encuenta errores numericos

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,6 @@
 #include "matrix.h"
 
-Matrix::Matrix(int n, int m) :  _arreglo(NULL), _filas(n), _columnas(m) {
+Matrix::Matrix(int n, int m) : _filas(n), _columnas(m) {
 	_arreglo = new double[_filas*_columnas];
 
 	// se rellena con 0
@@ -40,12 +40,25 @@ void Matrix::print() {
 	return;
 }
 
+
 Matrix::~Matrix() {
 	delete [] _arreglo;
 }
 
+
 Fila::Fila(double* arreglo, int columnas) : _arreglo(NULL),  _columnas(columnas) {
 	_arreglo = arreglo;
+}
+
+int Fila::columnas() {
+	return _columnas;
+}
+
+void Fila::operator=(Fila f) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		_arreglo[i] = f._arreglo[i];
+	} 
 }
 
 void Fila::sumar(Fila f) {
@@ -55,7 +68,35 @@ void Fila::sumar(Fila f) {
 	}
 }
 
+void Fila::operator+=(Fila f) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		_arreglo[i] += f._arreglo[i];
+	}
+}
+
+void Fila::sumar(double alpha) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		_arreglo[i] += alpha;
+	}
+}
+
+void Fila::operator+=(double alpha) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		_arreglo[i] += alpha;
+	}
+}
+
 void Fila::multiplicar(double alpha) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		_arreglo[i] *= alpha;
+	}
+}
+
+void Fila::operator*=(double alpha) {
 	for (int i = 0; i < _columnas; ++i)
 	{
 		_arreglo[i] *= alpha;
@@ -69,4 +110,78 @@ void Fila::sumar_multiplo(Fila f, double alpha) {
 	}
 }
 
+void Fila::swapear(Fila f) {
+	for (int i = 0; i < _columnas; ++i)
+	{
+		double temp = _arreglo[i];
+		_arreglo[i] = f._arreglo[i];
+		f._arreglo[i] = temp;
+	}
+}
 
+
+void triangular(Matrix &A, Matrix &b) {
+	// se realiza triangulacion usando cada uno de las filas como pivote
+	for (int pivote = 0; pivote < A.filas() - 1; ++pivote)
+	{
+		double aii = A(pivote,pivote);
+
+		// si este pivote no sirve para triangular
+		if (aii == 0)
+		{
+			// se busca otro pivote que tenga la columna i no nula
+			int posible_pivote = pivote + 1;
+			while(posible_pivote < A.filas() and A(posible_pivote,pivote) == 0) {
+				++posible_pivote;
+			}
+
+			// si se encontro alguno, se swapean
+			if (posible_pivote < A.filas())
+			{
+				A(pivote).swapear(A(posible_pivote));
+			}
+
+			// si no, entonces esta columna ya esta triangulada porque son todos 0.
+			else {continue;}
+			
+		}
+
+		// se aplica el pivote a cada una de las filas
+		for (int j = pivote+1; j < A.filas(); ++j)
+		{
+			double aji = A(j,pivote);
+			double alpha = -aji/aii;
+			A(j,pivote) = 0;
+
+			b(j).sumar_multiplo(b(pivote), alpha); 
+
+			// se aplica el pivote a cada componente de la fila.
+			for (int k = pivote+1; k < A.columnas(); ++k)
+			{
+				A(j,k) += A(pivote,k) * alpha;
+			}
+		}
+	}
+}
+
+
+// Devuelve la solucion del sistema.
+Matrix resolver_triangulado(Matrix &A, Matrix &b) {
+	Matrix X = Matrix(A.filas(), b.columnas());
+
+	for (int i = A.filas()-1; i >= 0; --i)
+	{
+		X(i) = b(i);
+
+		for (int j = A.columnas() - 1; j > i; --j)
+		{
+			double aij = A(i,j);
+			X(i).sumar_multiplo(X(j), -aij);
+		}
+
+		double aii = A(i,i);
+		X(i).multiplicar(1.0/aii);
+	}
+
+	return X;
+}
--- matrix.h
+++ matrix.h
@@ -6,14 +6,29 @@ using namespace std;
 // utilidad para hacer operaciones con filas bonito
 class Fila {
 	public:
-		// suma componente a componente. Supone mismo tamanio de fila 
+		int columnas();
+
+		// asignacion. Supone mismo numero de columnas
+		void operator=(Fila f);
+
+		// suma componente a componente. Supone mismo numero de columnas
 		void sumar(Fila f);
+		void operator+=(Fila f);
+
+		// sumar alpha a todos los componentes
+		void sumar(double alpha);
+		void operator+=(double alpha);
 
 		// multiplica todos los componentes por alpha
 		void multiplicar(double alpha);
+		void operator*=(double alpha);
 
-		// suma f*alpha a la fila.  Supone mismo tamanio de fila 
+		// suma f*alpha a la fila.  Supone mismo numero de columnas 
 		void sumar_multiplo(Fila f, double alpha);
+
+		// intercambia el contenido de esta fila con la fila f. Supone mismo numero de columnas
+		void swapear(Fila f);
+
 
 	private:
 		double* _arreglo; // donde comienza la fila
@@ -28,6 +43,7 @@ class Fila {
 class Matrix {
 	public:
 		Matrix(int n, int m);
+
 		int filas();
 		int columnas();
 		~Matrix();
