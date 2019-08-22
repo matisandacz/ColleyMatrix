8e3d4c442d2009e2c619fb2abbaca79ee6ea85bd
mgrynberg <grynberg8@gmail.com>
2019-08-22 14:44:16 -0300

    reordenado

---
+++ generate_ranking.cpp
@@ -0,0 +1,119 @@
+#include "matrix.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+Matrix colley(int T, vector<vector<int> >& partidos);
+
+Matrix wp(int T, vector<vector<int> >& partidos);
+
+Matrix nuestro_metodo(int T, vector<vector<int> >& partidos);
+
+int main(int argc, char** argv) {
+	if(argc != 4){
+	  cout << "Debe ingresar exactamente 3 parametros." << endl;
+	  exit(0);
+	}
+	//argv[1] es numero de metodo
+	//argv[2] es archivo de entrada
+	//argv[3] es archivo de salida
+	ifstream inputf(argv[2]);
+	if (inputf.is_open()) {
+	  //Cantidad de participantes - cantidad de partidos.
+	  int T, P;
+	  inputf >> T >> P;
+
+	  vector<vector<int> > partidos; //equipo0 equipo1 puntaje0 puntaje1
+	  vector<int> opcionales;
+	  int opcional, i, goles_i, j, goles_j;
+	  while (inputf >> opcional >> i >> goles_i >> j >> goles_j){
+	    vector<int> nuevo_partido;
+	    nuevo_partido.push_back(i);
+	    nuevo_partido.push_back(j);
+	    nuevo_partido.push_back(goles_i);
+	    nuevo_partido.push_back(goles_j);
+
+	    partidos.push_back(nuevo_partido);
+	    opcionales.push_back(opcional);
+	  }
+
+	  Matrix ranking;
+
+	  if(argv[1][0] == '0'){
+	    ranking = colley(T,partidos);
+	  } else if(argv[1][0] == '1'){
+	    ranking = wp(T,partidos);
+	  } else if(argv[1][0] == '2'){
+	    ranking = nuestro_metodo(T,partidos);
+	  } else {
+	    cout << "Metodo invalido." << endl;
+	    exit(0);
+	  }
+
+	  //Escribimos el ranking en el archivo de salida
+	  ofstream outputf(argv[3]);
+	  if (outputf.is_open()){
+	    for(int i = 0; i < T; i++)
+	      outputf << fixed << setprecision(15) << ranking(i,0) << "\n";
+	    cout << "Ranking generado con éxito." << endl;
+	  } else {
+	    cout << "No se pudo abrir el archivo de salida." << endl;
+	    exit(0);
+	  }
+	}
+	else {
+	  cout << "No se pudo abrir el archivo de entrada." << endl;
+	  exit(0);
+	}
+	return 0;
+}
+
+Matrix colley(int T, vector<vector<int> >& partidos){
+	//Creamos la matriz de Colley
+	Matrix C(T,T);
+	for(int k = 0; k < T; k ++)
+		C(k,k) = 2.0;
+
+	//Creamos el vector b
+	Matrix b(T,1);
+
+	//Cargamos los partidos
+	for(int k = 0; k < partidos.size(); k++){
+		int i = partidos[k][0];
+		int j = partidos[k][1];
+		C(i-1,i-1)++;
+		C(j-1,j-1)++;
+		C(i-1,j-1)--;
+		C(j-1,i-1)--;
+
+
+		if (partidos[k][2] > partidos[k][3]) {
+			b(i-1,0) += 1.0;
+			b(j-1,0) -= 1.0;
+		} else {
+			b(i-1,0) -= 1.0;
+			b(j-1,0) += 1.0;
+		}
+	}
+
+	for (int i = 0; i < T; i++) {
+		b(i,0) /= 2.0;
+		b(i,0) += 1.0;
+	}
+
+	//Guardamos en ranking la solucion del sistema Cx = b obtenida con Eliminacion Gaussiana
+	triangular(C,b);
+	Matrix ranking = resolver_triangulado(C,b);
+	return ranking;
+}
+
+Matrix wp(int T, vector<vector<int> >& partidos){
+	return Matrix(T,1);
+}
+
+Matrix nuestro_metodo(int T, vector<vector<int> >& partidos){
+	return Matrix(T,1);
+}
---
+++ medir_error.py
@@ -0,0 +1,92 @@
+import sys
+import os
+import subprocess
+import numpy as np
+
+def medir_error(archivo_partidos, archivo_ranking, verbose = False) :
+	partidos = open(archivo_partidos, "r")
+
+	## obtencion de matriz de colley y b
+	n_equipos =  int(partidos.readline().split()[0])
+	colley = np.identity(n_equipos, dtype = float)
+	colley *= 2
+	b =  np.zeros(n_equipos)	
+
+	for line in partidos:
+		palabras = line.split()
+		equipo_1 = int(palabras[1])
+		puntaje_1 = int(palabras[2])
+		equipo_2 = int(palabras[3])
+		puntaje_2 = int(palabras[4])	
+
+		colley[equipo_1-1][equipo_2-1] -=1
+		colley[equipo_2-1][equipo_1-1] -=1	
+
+		colley[equipo_1-1][equipo_1-1] += 1
+		colley[equipo_2-1][equipo_2-1] += 1	
+
+		if puntaje_1 > puntaje_2:
+			b[equipo_1-1] += 1.0
+			b[equipo_2-1] -= 1.0
+		else:
+			b[equipo_1-1] -= 1.0
+			b[equipo_2-1] += 1.0	
+	
+
+	b /= 2.0
+	b += 1.0
+
+	partidos.close()
+	f = open(archivo_ranking)	
+	
+
+	## leer matriz resultado
+	ranking = np.loadtxt(f, delimiter="\n")
+	f.close()
+
+	colley_inv = np.linalg.inv(colley)
+	ranking_verd = colley_inv.dot(b) 
+		
+	
+
+	if verbose:
+
+		print "Colley---------"
+		print colley		
+
+		print "b---------"
+		print b		
+
+		print "Ranking obtenido---------"
+		print ranking		
+
+
+		print "Ranking verdadero-----"
+		print ranking_verd		
+
+
+	acumulado = 0.0
+	for x in xrange(len(ranking_verd)):
+		acumulado += abs(ranking_verd[x]  - ranking [x])
+	return (acumulado/len(ranking_verd))
+
+
+def correr_competencias():
+	f = open("errores.txt", "w")
+	for n in xrange(10,100):
+		print n/(100.0-10.0)
+		epsilon = 0.0
+		N_DE_VECES_QUE_EJECUTA = 10
+
+		for repeticiones in xrange(N_DE_VECES_QUE_EJECUTA):
+			subprocess.call("python generador_competencia.py " + str(n) + " 0.5", shell=True)
+			subprocess.call("./main 0 competencia_generada.in ranking.out", shell=True)
+			epsilon +=  medir_error("competencia_generada.in", "ranking.out")
+
+		epsilon = epsilon/N_DE_VECES_QUE_EJECUTA
+		f.write(str(epsilon)+"\n")
+	f.close()
+
+correr_competencias()
+
+
