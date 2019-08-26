0f4332840ec95476200772e9535d834131a197b3
Migue <migue@Fainstein.home>
2019-08-26 11:40:37 -0300

    sensibilidad

--- generate_ranking.cpp
+++ generate_ranking.cpp
@@ -6,15 +6,8 @@
 #include <iomanip>
 #include <math.h>
 #include <map>
+#include "metodos.h"
 using namespace std;
-
-Matrix colley(int T, vector<vector<int> >& partidos, char arg);
-
-Matrix wp(int T, vector<vector<int> >& partidos);
-
-Matrix nuestro_metodo(int T, vector<vector<int> >& partidos, char arg);
-
-double f(int d,int T, char arg); //funcion de asignacion de puntaje de nuestro metodo
 
 int main(int argc, char** argv) {
 	if(argc != 4 && argc != 5){
@@ -52,7 +45,7 @@ int main(int argc, char** argv) {
 			else
 	    	ranking = colley(T,partidos,'0');
 	  } else if(argv[1][0] == '1'){
-	    ranking = wp(T,partidos);
+	    ranking = wp(T,partidos, '0');
 	  } else if(argv[1][0] == '2'){
 			if(argc == 5)
 	    	ranking = nuestro_metodo(T,partidos,argv[4][0]);
@@ -83,138 +76,3 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-Matrix colley(int T, vector<vector<int> >& partidos, char arg){
-	//Creamos la matriz de Colley
-	Matrix C(T,T);
-	for(int k = 0; k < T; k ++)
-		C(k,k) = 2.0;
-
-	//Creamos el vector b
-	Matrix b(T,1);
-
-	//Cargamos los partidos
-	for(int k = 0; k < partidos.size(); k++){
-		int i = partidos[k][0];
-		int j = partidos[k][1];
-		C(i-1,i-1)++;
-		C(j-1,j-1)++;
-		C(i-1,j-1)--;
-		C(j-1,i-1)--;
-
-		if (partidos[k][2] > partidos[k][3]) {
-			b(i-1,0) += 1.0;
-			b(j-1,0) -= 1.0;
-		} else {
-			b(i-1,0) -= 1.0;
-			b(j-1,0) += 1.0;
-		}
-	}
-
-	for (int i = 0; i < T; i++) {
-		b(i,0) /= 2.0;
-		b(i,0) += 1.0;
-	}
-
-	//Guardamos en ranking la solucion del sistema Cx = b obtenida con Eliminacion Gaussiana
-	triangular(C,b);
-	Matrix ranking = resolver_triangulado(C,b);
-
-	if(arg == '1'){
-		Matrix bPrima = C*ranking;
-		ofstream outputf("error_numerico.csv");
-	  if (outputf.is_open()){
-	  	double epsilon = 0.0;
-	    for(int i = 0; i < T; i++){
-				double error_act = b(i,0) - bPrima(i,0);
-				if(error_act < 0) {
-					error_act = -error_act;
-				}
-				epsilon += error_act;
-			}
-		outputf << fixed << setprecision(15) << (epsilon/ ((double)T) ) << "\n";
-	    cout << "Error de aproximacion guardado con éxito." << endl;
-	  } else {
-	    cout << "No se pudo abrir el archivo de salida de errores." << endl;
-	    exit(0);
-	  }
-	}
-	return ranking;
-}
-
-Matrix wp(int T, vector<vector<int> >& partidos){
-	//T es la cantidad de equipos
-	//partidos contiene vectores <EQUPO0 EQUIPO1  puntaje0 puntaje1>
-	//Creamos un diccionario infoJugadores que dado un equipo i, nos da un vector <#GANADOS, #TOTAL_JUGADOS>
-	vector<pair<int,int> > infoJugadores;
-	for(int i = 0; i < T; i++)
-		infoJugadores.push_back(pair<int,int>(0,0)); //al principio nadie jugo nada
-	for(int i = 0; i < partidos.size(); i++){
-		int equipo0 =  partidos[i][0] - 1;
-		int equipo1 =  partidos[i][1] - 1;
-		int puntaje0 =  partidos[i][2];
-		int puntaje1 =  partidos[i][3];
-		/*CASO GANA EL EQUIPO 0*/
-		if(puntaje0 > puntaje1){
-			//Sumamos un ganado al primer equipo
-			infoJugadores[equipo0].first +=1;
-			//Sumamos un partido jugado al primer equipo
-			infoJugadores[equipo0].second +=1;
-			//Sumamos un partido jugado al segundo equipo
-			infoJugadores[equipo1].second +=1;
-		}
-			/*CASO GANA EL EQUIPO 1*/
-		else{
-			//Sumamos un ganado al segundo equipo
-			infoJugadores[equipo1].first +=1;
-			//Sumamos un partido jugado al primer equipo
-			infoJugadores[equipo0].second +=1;
-			//Sumamos un partido jugado al segundo equipo
-			infoJugadores[equipo1].second +=1;
-		}
-	}
-	//Creamos la matriz de rankings de acuerdo a #Ganados/#Total_jugados
-	Matrix ranking = Matrix(T,1);
-
-	for (int i = 0; i < T; i++) {
-		if(infoJugadores[i].second != 0){
-			ranking(i,0) = (double)infoJugadores[i].first / (double)infoJugadores[i].second;
-		}else
-			ranking(i,0) = -1; //-1 si no se puede calcular porque todavia no jugo
-	}
-
-	return ranking;
-}
-
-Matrix nuestro_metodo(int T, vector<vector<int> >& partidos, char arg){
-	Matrix tabla(T,1); //puntaje de cada equipo, al principio es 0
-	for(int i = 0; i < partidos.size(); i++){
-		int rank1 = 1;
-		int rank2 = 1;
-		for(int j = 0; j < T; j++){
-			if(tabla(j,0) > tabla(partidos[i][0]-1,0)) rank1++;
-			if(tabla(j,0) > tabla(partidos[i][1]-1,0)) rank2++;
-		}
-		if(partidos[i][1] > partidos[i][3]){
-			tabla(partidos[i][0],0) += f(rank1 - rank2, T, arg);
-			tabla(partidos[i][2],0) -= f(rank1 - rank2, T, arg);
-		} else {
-			tabla(partidos[i][0],0) += f(rank2 - rank1, T, arg);
-			tabla(partidos[i][2],0) -= f(rank2 - rank1, T, arg);
-		}
-	}
-	return tabla;
-}
-
-double f(int d, int T, char arg){ //si type es true es exponencial y si es false es sigmoidea
-	//cout << arg << endl;
-	if(arg == '0'){
-		//cout << "Exponencial" << endl;
-		return exp(d*(log(T)/(double)T)); //no me pregunten por que, fueron los parametros que mas me gustaron
-	} else if(arg == '1'){
-		//cout << "Sigmoidea" << endl;
-		return 1.0/(1.0+exp(-d*(log(T)/(double)T)));
-	} else {
-		//cout << "Lineal" << endl;
-		return 0.5+(2.0/(double)T)*(double)d;
-	}
-}
---
+++ sensibilidad.cpp
@@ -0,0 +1,167 @@
+#include "matrix.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include "metodos.h"
+
+using namespace std;
+
+bool mayor(double i, double j){return i > j;}
+
+//funcion que dado un ranking y una posicion, da el numero de equipo que se encuentra
+// en dicha posicion.
+int equipoEnPos(const Matrix& rank, int pos){
+	vector<double> posiciones;
+	for(int i = 0; i < rank.filas() ; ++i){
+		posiciones.push_back(rank(i,0));
+	}
+	vector<double> copia(posiciones.begin(), posiciones.end());
+	stable_sort(posiciones.begin(), posiciones.end(), mayor);
+	int res = 0;
+	while(copia[res] != posiciones[pos - 1] && res < copia.size()){
+		res++;
+	}
+	return res;
+}
+
+//funcion que dado un ranking y un equipo, devuelve su posicion en la tabla ordenada
+int posEquipo(const Matrix& rank, int equipo){
+	double rankEquipo = rank(equipo,0);
+	vector<double> posiciones;
+	for(int i = 0; i < rank.filas() ; ++i){
+		posiciones.push_back(rank(i,0));
+	}
+	stable_sort(posiciones.begin(), posiciones.end(), mayor);
+
+	int res = 0;
+	while(posiciones[res] != rankEquipo && res < posiciones.size()){
+		++res;
+	}
+
+	return res;
+}
+
+// funcion que dado un equipo, la cantidad de equipos, una función que calcula un ranking, y una secuencia de partidos; determina
+//		el salto promedio que realizaria dicho equipo en el partido siguiente, si le ganara al resto de los equipos
+double promedioSensibilidad(int equipo, int cantEquipos, Matrix (*metodo)(int, vector<vector<int> >&, char) , vector<vector<int> >& partidos){
+	double res = 0;
+	Matrix rankInicial;
+	rankInicial = metodo(cantEquipos, partidos, '0');
+	int posInicial = posEquipo(rankInicial, equipo);
+	for(int i = 0 ; i < cantEquipos ; i++){
+		if(i != equipo){
+			vector<int> nuevo_partido;
+			nuevo_partido.push_back(equipo);
+			nuevo_partido.push_back( i);
+			nuevo_partido.push_back( 1);
+			nuevo_partido.push_back( 0);
+			partidos.push_back(nuevo_partido);
+
+			Matrix rankNuevo;
+			rankNuevo = metodo(cantEquipos, partidos, '0');
+
+			int posNueva = posEquipo(rankNuevo, equipo);
+
+			res += posNueva - posInicial;
+			partidos.pop_back();
+		}
+	}
+
+	return res/(cantEquipos - 1);
+}
+
+int main(int argc, char** argv) {
+	if(argc != 4){
+	  cout << "Debe ingresar exactamente 3 parametros." << endl;
+	  exit(0);
+	}
+	//argv[1] es numero de metodo
+	//argv[2] es archivo de entrada
+	//argv[3] es archivo de salida
+	char met = argv[1][0];
+	ifstream inputf(argv[2]);
+	if (inputf.is_open()) {
+		//Cantidad de participantes - cantidad de partidos.
+		int T, P;
+		inputf >> T >> P;
+
+		vector<vector<int> > partidos; //equipo0 equipo1 puntaje0 puntaje1
+		int opcional, i, goles_i, j, goles_j;
+
+		double promedios[P][3];
+		int indicePartido = 0;
+
+		while (inputf >> opcional >> i >> goles_i >> j >> goles_j){
+			vector<int> nuevo_partido;
+			nuevo_partido.push_back(i);
+			nuevo_partido.push_back(j);
+			nuevo_partido.push_back(goles_i);
+			nuevo_partido.push_back(goles_j);
+
+			partidos.push_back(nuevo_partido);
+
+			if(met == '0'){
+				Matrix matCol = colley(T, partidos, '0');
+				int equipoMejor = equipoEnPos(matCol, 1);
+				int equipoInter = equipoEnPos(matCol, T/2);
+				int equipoPeor = equipoEnPos(matCol, T);
+
+				promedios[indicePartido][0] = promedioSensibilidad(equipoMejor, T, colley, partidos);
+				promedios[indicePartido][1] = promedioSensibilidad(equipoMejor, T, colley, partidos);
+				promedios[indicePartido][2] = promedioSensibilidad(equipoMejor, T, colley, partidos);
+			}
+			else{
+				if(met == '1'){
+					Matrix matWP = wp(T, partidos, '0');
+					int equipoMejor = equipoEnPos(matWP, 1);
+					int equipoInter = equipoEnPos(matWP, T/2);
+					int equipoPeor = equipoEnPos(matWP, T);
+
+					promedios[indicePartido][0] = promedioSensibilidad(equipoMejor, T, wp, partidos);
+					promedios[indicePartido][1] = promedioSensibilidad(equipoMejor, T, wp, partidos);
+					promedios[indicePartido][2] = promedioSensibilidad(equipoMejor, T, wp, partidos);
+				}
+				else{
+					if(met == '2'){
+						Matrix matNM = nuestro_metodo(T, partidos, '0');
+						int equipoMejor = equipoEnPos(matNM, 1);
+						int equipoInter = equipoEnPos(matNM, T/2);
+						int equipoPeor = equipoEnPos(matNM, T);
+
+						promedios[indicePartido][0] = promedioSensibilidad(equipoMejor, T, nuestro_metodo, partidos);
+						promedios[indicePartido][1] = promedioSensibilidad(equipoMejor, T, nuestro_metodo, partidos);
+						promedios[indicePartido][2] = promedioSensibilidad(equipoMejor, T, nuestro_metodo, partidos);
+					}
+					else{
+						cout << "Metodo no valido" << endl;
+					}
+				}
+			}
+			indicePartido++;
+		}
+
+		ofstream outputf(argv[3]);
+	    if (outputf.is_open()){
+			for(int i = 0; i < T; i++){
+				outputf << fixed << setprecision(15) << promedios[i][0] << "  -   ";
+				outputf << fixed << setprecision(15) << promedios[i][1] << "  -   ";
+				outputf << fixed << setprecision(15) << promedios[i][2] << "\n";
+			}
+			cout << "Ranking generado con éxito." << endl;
+	    } else {
+		      cout << "No se pudo abrir el archivo de salida." << endl;
+		      exit(0);
+	    }
+			
+	}
+	else {
+	  cout << "No se pudo abrir el archivo de entrada." << endl;
+	  exit(0);
+	}
+	return 0;
+}
+
+
